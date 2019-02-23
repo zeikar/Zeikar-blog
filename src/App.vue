32a91557ce97bf196985056138b2264b5ebc8ed1
Zeikar <antstp@naver.com>
2019-02-23 19:57:21 +0900

    Install bulma, add sample code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Articles/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bulma/css/bulma.css'
+import Articles from './components/Articles.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Articles
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Articles.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="notification">This container is
+      <strong>centered</strong> on desktop.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Articles",
+  props: {
+    msg: String
+  }
+};
+</script>
