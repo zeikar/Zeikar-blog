c83e0384fb29eae2041ac04d614f9f793a6377d6
Zeikar <antstp@naver.com>
2019-02-25 23:15:02 +0900

    Add Typewriter effect to Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,38 @@
 <template>
   <section class="hero home-background has-background is-fullheight-with-navbar">
     <div class="hero-body">
-      <div class="container">
-        <p class="title">반갑습니다.</p>
-        <p>Zeikar의 블로그입니다.</p>
+      <div class="container is-size-1 has-text-centered">
+        <vue-typer
+          :text="['Hello.','I am Zeikar.','I Love to Develop.','Thank you for visiting my blog!']"
+          :repeat="Infinity"
+          :shuffle="false"
+          initial-action="typing"
+          :pre-type-delay="70"
+          :type-delay="100"
+          :pre-erase-delay="2000"
+          :erase-delay="70"
+          erase-style="backspace"
+          :erase-on-complete="false"
+          caret-animation="smooth"
+        ></vue-typer>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import { VueTyper } from "vue-typer";
+
 export default {
-    name: "Home"
+  name: "Home",
+  components: {
+    VueTyper
+  }
 };
 </script>
 
 <style>
 .home-background {
-    background-image: url("https://images.pexels.com/photos/872958/pexels-photo-872958.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+  background-image: url("https://images.pexels.com/photos/872958/pexels-photo-872958.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
 }
 </style>
