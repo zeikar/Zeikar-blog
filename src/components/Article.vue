c1de2e6c17c37ce997db85149bc242a4abf26403
Zeikar <antstp@naver.com>
2019-03-27 22:52:51 +0900

    Change Articles UI (image left align)

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,14 +1,15 @@
 <template>
-  <article class="media">
-    <div class="media-content">
+  <div class="columns is-desktop">
+    <div class="column has-text-centered">
+      <img class="article-image" :src="post.thumbnail">
+    </div>
+    <div class="column is-four-fifths">
       <nav class="level">
         <div class="content">
           <h2>
             <router-link class="has-text-dark" :to="'/posts/' + post.title">{{post.title}}</router-link>
           </h2>
-          <p>
-            {{contentSubstring()}}
-          </p>
+          <p class="article-content">{{contentSubstring()}}</p>
           <p>
             <span class="icon">
               <i class="fas fa-clock"></i>
@@ -18,12 +19,7 @@
         </div>
       </nav>
     </div>
-    <figure class="media-right">
-      <p class="image is-128x128">
-        <img style="object-fit: cover;" :src="post.thumbnail">
-      </p>
-    </figure>
-  </article>
+  </div>
 </template>
 
 <script>
@@ -36,8 +32,32 @@ export default {
       return date.toLocaleDateString();
     },
     contentSubstring() {
-      return this.post.contents.substring(1, 150) + "...";
+      return this.post.contents.substring(1, 300) + "...";
     }
   }
 };
 </script>
+
+<style>
+.article-content {
+  line-height: 24px;
+  display: block;
+  max-height: 48px; /* 2 lines */
+  display: -webkit-box; /* multi-line ellipsis */
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  overflow: hidden;
+}
+
+.article-image {
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  color: #4a4a4a;
+  padding: 0.25rem;
+
+  height: 128px;
+}
+</style>
